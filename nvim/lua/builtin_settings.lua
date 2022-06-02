0682f5bee3d483237f0cf150a8a96fa0c4360828
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2022-06-01 18:03:22 -0700

    added some lua

---
+++ nvim/lua/builtin_settings.lua
@@ -0,0 +1,13 @@
+vim.g.shell = 'bash'
+vim.g.termguicolors = true
+vim.g.shiftwidth = 4
+vim.g.tabstop = 4
+vim.g.expandtab = false
+vim.g.smarttab = true
+vim.g.smartindent = true
+vim.g.softtabstop = 4
+vim.g.cindent = true
+vim.g.autoindent = true
+
+vim.g.signcolumn = 'yes'
+vim.g.relativenumber = true
