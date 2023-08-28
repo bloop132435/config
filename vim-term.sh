7326b3f910cc3116acf1eef4c9f3795fdfefae6b
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2023-08-27 20:37:05 -0700

    added guard statement around python aliases

--- vim-term.sh
+++ vim-term.sh
@@ -1,5 +1,8 @@
 source ~/.bashrc
 alias nvim=nvr
 #  conda activate base
-alias python=/opt/homebrew/Caskroom/miniforge/base/bin/python
-alias python3=/opt/homebrew/Caskroom/miniforge/base/bin/python3
+if [ -d /opt/homebrew/Caskroom/miniforge ]
+then
+	alias python=/opt/homebrew/Caskroom/miniforge/base/bin/python
+	alias python3=/opt/homebrew/Caskroom/miniforge/base/bin/python3
+fi
